9f40ab82efaa0fafb0b4098f1fd041ff80ca55e8
Baladi_Imane <72796883+Imane-code-ux@users.noreply.github.com>
2021-06-10 13:23:31 +0100

    Update mandelbrot.c

--- BALADI_Imane/mandelbrot.c
+++ BALADI_Imane/mandelbrot.c
@@ -19,33 +19,25 @@ void mandelbrot(double x,double y,SDL_Renderer *renderer)
 	// on calcule la taille de l'image :
 	double image_x = (x2 - x1) * zoom;
 	double image_y = (y2 - y1) * zoom;
-	x=0;
-	while( x < image_x)
+        double c_r = x / zoom + x1;
+        double c_i = y / zoom + y1;
+        double z_r = 0;
+        int z_i = 0;
+        int i = 0;
+	while( z_r*z_r + z_i*z_i < 4 && i < iteration_max)
 	{
-    		int y=0;
-		while(y < image_y)
-		{
-        		double c_r = x / zoom + x1;
-        		double c_i = y / zoom + y1;
-        		double z_r = 0;
-        		int z_i = 0;
-        		int i = 0;
-			while( z_r*z_r + z_i*z_i < 4 && i < iteration_max)
-			{
-				double tmp = z_r;
-            			z_r = z_r*z_r - z_i*z_i + c_r;
-            			z_i = 2*z_i*tmp + c_i;
-            				i = i+1;
-			}	
-			if(i==iteration_max)
-				
-			{
-				light_pixel( x, y ,renderer);			
-			}	
-			y+=1;
-		}
-		x+=1;
+		double tmp = z_r;
+            	z_r = z_r*z_r - z_i*z_i + c_r;
+            	z_i = 2*z_i*tmp + c_i;
+            	i = i+1;
 	}	
+	if(i==iteration_max)
+				
+	{
+		light_pixel( x, y ,renderer);			
+	}	
+		
+		
 	
 }
 
